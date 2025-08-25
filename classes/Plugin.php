7003a28ee2103b9bef1012e5451721bb3bacc3e6
Christoph M. Becker <cmbecker69@gmx.de>
2025-08-25 15:17:46 +0200

    Drop annoying license info from plugin info screen

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -47,41 +47,12 @@ class Plugin
         $o .= print_plugin_admin('off');
         switch ($admin) {
             case '':
-                $o .= $this->renderVersion() . "<hr>"
+                $o .= '<h1>Tablesorter ' . self::VERSION . '</h1>'
                     . $this->renderSystemCheck();
                 break;
             default:
                 $o .= plugin_admin_common($action, $admin, 'tablesorter');
         }
-    }
-
-    /**
-     * @return string
-     */
-    protected function renderVersion()
-    {
-        global $pth, $plugin_tx;
-
-        return '<h1>Tablesorter</h1>'
-            . '<img class="tablesorter_logo" src="' . $pth['folder']['plugins']
-            . 'tablesorter/tablesorter.png" alt="' . $plugin_tx['tablesorter']['alt_logo'] . '">'
-            . '<p style="margin-top: 1em">Version: ' . self::VERSION . '</p>'
-            . '<p>Copyright &copy; 2012-2019 Christoph M. Becker</p>'
-            . '<p class="tablesorter_license">'
-            . 'Tablesorter_XH is free software: you can redistribute it and/or modify'
-            . ' it under the terms of the GNU General Public License as published by'
-            . ' the Free Software Foundation, either version 3 of the License, or'
-            . ' (at your option) any later version.</p>'
-            . '<p class="tablesorter_license">'
-            . 'Tablesorter_XH is distributed in the hope that it will be useful,'
-            . ' but <em>without any warranty</em>; without even the implied warranty of'
-            . ' <em>merchantability</em> or <em>fitness for a particular purpose</em>.  See the'
-            . ' GNU General Public License for more details.</p>'
-            . '<p class="tablesorter_license">'
-            . 'You should have received a copy of the GNU General Public License'
-            . ' along with Tablesorter_XH.  If not, see'
-            . ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/'
-            . '</a>.</p>';
     }
 
     /**
@@ -97,7 +68,7 @@ class Plugin
         $ok = '<img src="' . $imgdir . 'ok.png" alt="ok">';
         $warn = '<img src="' . $imgdir . 'warn.png" alt="warning">';
         $fail = '<img src="' . $imgdir . 'fail.png" alt="failure">';
-        $o = '<hr>' . '<h4>' . $ptx['syscheck_title'] . '</h4>'
+        $o = '<h4>' . $ptx['syscheck_title'] . '</h4>'
             . (version_compare(PHP_VERSION, $phpVersion) >= 0 ? $ok : $fail)
             . '&nbsp;&nbsp;'
             . sprintf($ptx['syscheck_phpversion'], $phpVersion)
