e965f82625d36573f1a7eb0b8e5195a5bfd81512
Christoph M. Becker <cmbecker69@gmx.de>
2025-08-25 19:35:10 +0200

    Add basic test for Main

---
+++ tests/MainTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tablesorter;
+
+use ApprovalTests\Approvals;
+use PHPUnit\Framework\TestCase;
+use Plib\View;
+
+class MainTest extends TestCase
+{
+    /** @var array<string,string> */
+    private array $config;
+
+    private View $view;
+
+    protected function setUp(): void
+    {
+        $this->config = XH_includeVar("./config/config.php", "plugin_cf")["tablesorter"];
+        $this->view = new View("./views/", XH_includeVar("./languages/en.php", "plugin_tx")["tablesorter"]);
+    }
+
+    private function sut(): Main
+    {
+        return new Main("./plugins/tablesorter/", $this->config, $this->view);
+    }
+
+    public function testRendersScript(): void
+    {
+        $response = $this->sut()();
+        Approvals::verifyHtml($response->bjs());
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 require_once "../../cmsimple/functions.php";
 
+require_once "../plib/classes/Response.php";
 require_once "../plib/classes/SystemChecker.php";
 require_once "../plib/classes/View.php";
 require_once "../plib/classes/FakeSystemChecker.php";
